fbddf2fd4a7b619d33225356519b0361949563b3
Greg Quick <gquick30@gmail.com>
2025-12-31 04:09:56 +0000

    create-customer added code for linking email

--- api/create-customer.ts
+++ api/create-customer.ts
@@ -44,6 +44,19 @@ export default async function handler(req, res) {
 
   const data = await response.json();
 
-  // Ignore duplicate email errors
+  const errs = data?.errors?.map((e: any) => e?.message).filter(Boolean) || [];
+  const userErrs =
+    data?.data?.customerCreate?.customerUserErrors
+      ?.map((e: any) => e?.message)
+      .filter(Boolean) || [];
+
+  if (!response.ok || errs.length || userErrs.length) {
+    return res.status(400).json({
+      success: false,
+      errs,
+      userErrs,
+    });
+  }
+
   return res.status(200).json({ success: true });
 }
